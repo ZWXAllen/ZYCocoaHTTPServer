5caa0cbee141d037074cbcc3ee547ff7bf3d4907
Allen <young.zhang@ecovacs.com>
2021-05-21 15:05:17 +0800

    update

--- ZYCocoaHTTPServer/Classes/IRCocoaHTTPServer.h
+++ ZYCocoaHTTPServer/Classes/IRCocoaHTTPServer.h
@@ -13,10 +13,10 @@ FOUNDATION_EXPORT double IRCocoaHTTPServerVersionNumber;
 //! Project version string for IRCocoaHTTPServer.
 FOUNDATION_EXPORT const unsigned char IRCocoaHTTPServerVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <IRCocoaHTTPServer/PublicHeader.h>
+// In this header, you should import all the public headers of your framework using statements like #import <ZYCocoaHTTPServer/PublicHeader.h>
 
-#import <IRCocoaHTTPServer/HTTPServer.h>
-#import <IRCocoaHTTPServer/HTTPConnection.h>
-#import <IRCocoaHTTPServer/HTTPMessage.h>
-#import <IRCocoaHTTPServer/HTTPResponse.h>
-#import <IRCocoaHTTPServer/HTTPDataResponse.h>
+#import <ZYCocoaHTTPServer/HTTPServer.h>
+#import <ZYCocoaHTTPServer/HTTPConnection.h>
+#import <ZYCocoaHTTPServer/HTTPMessage.h>
+#import <ZYCocoaHTTPServer/HTTPResponse.h>
+#import <ZYCocoaHTTPServer/HTTPDataResponse.h>
